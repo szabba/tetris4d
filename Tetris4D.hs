d2fc9cef1ee44df31bcfd3c8d79b7a9aa0ad45b6
Karol Marcjan <karol.marcjan@gmail.com>
2012-04-29 23:30:05 +0200

    Turned points form (Int, [Int]) to plain [Int] and made all thus required modifications.
    Also fixed a previously unnoticed issue caused by misguessing of the typeclass
    instance implementation for type (i, i).

--- Tetris4D.hs
+++ Tetris4D.hs
@@ -9,17 +9,21 @@ import Graphics.UI.SDL.Image as SDLi
 
 
 -- Geometry types
-type Point = (Int,[Int])
+type Point = [Int]
 type Block = [Point]
 type Well  = [Point]
-type Size  = Point
-type World  = (Well, Block)
+type World = (Well, Block)
 
 
 -- Game logic functions
 
 blockedBy :: Point -> Point -> Bool
-(y1, r1) `blockedBy` (y2, r2) = y1 - 1 == y2 && all (uncurry (==)) (zip r1 r2)
+p1 `blockedBy` p2 = y1 - 1 == y2 && r1 == r2
+  where
+    y1 = head p1
+    y2 = head p2
+    r1 = tail p1
+    r2 = tail p2
 
 stopsAt :: Block -> Point -> Bool
 block `stopsAt` point = any (`blockedBy` point) block
@@ -28,35 +32,21 @@ canFallFurther :: Block -> Well -> Bool
 block `canFallFurther` well = any (block `stopsAt`) well
 
 
--- Point arithmetics
+-- Translation
 
 add :: Point -> Point -> Point
-(y1, r1) `add` (y2, r2) = (y1 + y2, map (uncurry (+)) (zip r1 r2))
+p1 `add` p2 = zipWith (+) p1 p2
 
-
+ 
 -- All possible blocks!
 
 zeros :: Int -> [Int]
 zeros n = take n $ repeat 0
 
 neumann :: Int -> [Point]
-neumann n = (1, zeros $ n - 1) : [
-  (0, zeros i ++ [1] ++ zeros (n - 2 - i))
-  | i <- [0..(n-2)]] ++ [
-  (0, zeros i ++ [-1] ++ zeros (n - 2 - i))
-  | i <- [0..(n-2)]]
-
-shapesFrom :: Int -> Block -> [Block]
-shapesFrom n block = do
-  let lastPoint = head block
-  dp <- neumann n
-  let new = lastPoint `add` dp
-      previous = tail block
-  if new `elem` previous
-    then []
-    else [new : block]
-
-blockShapes' n blocks = foldl (++) [] $ map (shapesFrom n) blocks
+neumann n = [
+  zeros i ++ [ 1] ++ zeros (n - 1 - i) | i <- [0..(n-1)]] ++ [
+  zeros i ++ [-1] ++ zeros (n - 1 - i) | i <- [0..(n-1)]]
 
   -- This one's quite general.
 mapply :: (Integral i) => i -> (t -> t) -> t -> t
@@ -65,28 +55,52 @@ mapply n f a = mapply (n - 1) f (f a)
 
   -- dimmensions -> pointsInBlock -> legalBlocks
 blockShapes :: Int -> Int -> [Block]
-blockShapes n l = mapply (l - 1) (blockShapes' n) [[(0, zeros (n - 1))]]
+blockShapes n l = map reverse $ mapply (l - 1) (blockShapes' n) [[zeros n]]
+  where
+    blockShapes' :: Int -> [Block] -> [Block]
+    blockShapes' n blocks = foldl (++) [] $ map (shapesFrom n) blocks
+
+    shapesFrom :: Int -> Block -> [Block]
+    shapesFrom n block = do
+      let lastPoint = head block
+      dp <- neumann n
+      let new = lastPoint `add` dp
+          previous = tail block
+      if new `elem` previous
+        then []
+        else [new : block]
 
 
 -- Crossecting 
-type XSection = [(Int, Int)]
-type View = (XSection, XSection)
+--
+-- Beware as this part is specifically 4d while the code before was
+-- n-dimmensional.
+
+type XYPoint  = (Int, Int)
+type XYSection = [(Int, Int)]
+type View     = (XYSection, XYSection)
 type FullView = [View]
 
-view :: World -> (Int, Int) -> View
-view world (i, j) = fmap (filterOut i j) world
-
 filterOut :: Int -> Int -> [Point] -> [(Int, Int)]
-filterOut i j points = map dropZO $ filter (zoIS i j) points
+filterOut i j points = map dropZO $ filter (zoIs i j) points
   where
-    zoIS i j (y, [x, z, o]) = i == z && j == o
-    dropZO (y, x:rest) = (y, x)
+    zoIs :: Int -> Int -> Point -> Bool
+    zoIs i j [y, x, z, o] = i == z && j == o
+
+    dropZO :: Point -> (Int, Int)
+    dropZO (y:x:_) = (y, x)
+
+view :: World -> (Int, Int) -> View
+view (well, block) (i, j) = (filterOut i j well, filterOut i j block)
 
 fullView :: (Int, Int) -> World -> FullView
-fullView (zLen, oLen) world = map (view world) [(i, j) | i <- [0..(zLen-1)], j <- [0..(oLen-1)]]
-  
+fullView (zLen, oLen) world = map (view world) [(i, j)
+    | i <- [0..(zLen-1)], j <- [0..(oLen-1)]]
+   
 
+-- Rotation
 -- Painting
+-- Event handling
 -- Main function
 
   -- Bare in mind that this is a placeholder.
