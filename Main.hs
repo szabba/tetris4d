ecad184dc6ffcba1de6bd7bd3f7252d7172f4924
Karol Marcjan <karol.marcjan@gmail.com>
2012-05-25 04:33:50 +0200

    Further work on the event handling system.
    Note to myself: draw some flow diagram or smth for the current `menu` function.

--- Main.hs
+++ Main.hs
@@ -12,32 +12,63 @@ import Logic
 
 -- Event handling {{{1
 
-    -- Types
+    -- Types {{{2
 data Handler = Handler (HandlerFunc)
 
 type HandlerFunc = SDL.Event -> [Handler] -> IO [Handler]
 
+    -- Event loop {{{2
 eventLoop :: [Handler] -> IO ()
 eventLoop []     = return ()
-eventLoop (Handler h:hs) = do
+eventLoop hs@(Handler h:_) = do
     event <- pollEvent
     newHs <- h event hs
     eventLoop newHs
 
-    -- HandlerFuncs
+    -- Helpers {{{2
+one :: HandlerFunc -> IO [Handler]
+one hf = return [Handler hf]
+
+push :: HandlerFunc -> [Handler] -> IO [Handler]
+push hf hs = return (Handler hf : hs)
+
+drop' :: Int -> [Handler] -> IO [Handler]
+drop' n = return . drop n
+
+replaceTop :: HandlerFunc -> [Handler] -> IO [Handler]
+replaceTop hf (h:hs) = return $ Handler hf : hs
+
+    -- HandlerFuncS {{{2
 exit :: HandlerFunc
 exit _ _ = return []
 
-menu :: [String] -> HandlerFunc
-menu opts (KeyDown key) [] = do
-    if SDL.symKey key == SDL.SDLK_SPACE
-      then do
-        putStrLn $ head opts
-        return [Handler $ menu (tail opts)]
-      else return [Handler $ menu opts]
-menu opts _             [] = do
-    return [Handler $ menu opts]
+    -- The name is misleading -- it will actually block the program!
+doNothing :: HandlerFunc
+doNothing _ hs = return hs
 
+    -- While this one will go back to it's pusher.
+dropTop :: HandlerFunc
+dropTop event hs = do
+    pushEvent event
+    drop' 1 hs
+
+menu :: [(String, HandlerFunc)] -> HandlerFunc
+menu opts (KeyDown key) hs = act key'
+  where
+    key' = symKey key
+
+    (_, hf) = head opts
+    (name, _) = opts !! 1
+    rest = (drop 1 . cycle) opts
+
+    act SDL.SDLK_SPACE  = do
+        putStrLn $ name
+        one $ menu rest
+
+    act SDL.SDLK_RETURN = push hf hs
+    act _               = one $ menu rest
+        
+menu opts _             _ = one $ menu opts
 
 
 -- Main function {{{1
@@ -55,4 +86,5 @@ main = do
     SDL.blitSurface blockI Nothing screen Nothing
     SDL.flip screen
 
-    eventLoop [Handler $ menu $ cycle ["One", "Two", "Three"]]
+    handlerFuncS <- one $ menu [("One", dropTop), ("Exit", exit)]
+    eventLoop handlerFuncS
