ca9cf216bf6080f083995cd857ee53aadbf062d8
Karol Marcjan <karol.marcjan@gmail.com>
2012-05-25 09:43:24 +0200

    Moved the event handling code to a separate file.

--- Main.hs
+++ Main.hs
@@ -8,70 +8,10 @@ import Graphics.UI.SDL.Image as SDLi
 
 
 import Logic
+import Events
 
 
--- Event handling {{{1
-
-    -- Types {{{2
-data Handler = Handler (HandlerFunc)
-
-type HandlerFunc = SDL.Event -> [Handler] -> IO [Handler]
-
-    -- Event loop {{{2
-eventLoop :: [Handler] -> IO ()
-eventLoop []     = return ()
-eventLoop hs@(Handler h:_) = do
-    event <- pollEvent
-    newHs <- h event hs
-    eventLoop newHs
-
-    -- Helpers {{{2
-one :: HandlerFunc -> IO [Handler]
-one hf = return [Handler hf]
-
-push :: HandlerFunc -> [Handler] -> IO [Handler]
-push hf hs = return (Handler hf : hs)
-
-drop' :: Int -> [Handler] -> IO [Handler]
-drop' n = return . drop n
-
-replaceTop :: HandlerFunc -> [Handler] -> IO [Handler]
-replaceTop hf (h:hs) = return $ Handler hf : hs
-
-    -- HandlerFuncS {{{2
-exit :: HandlerFunc
-exit _ _ = return []
-
-    -- The name is misleading -- it will actually block the program!
-doNothing :: HandlerFunc
-doNothing _ hs = return hs
-
-    -- While this one will go back to it's pusher.
-dropTop :: HandlerFunc
-dropTop event hs = do
-    pushEvent event
-    drop' 1 hs
-
-menu :: [(String, HandlerFunc)] -> HandlerFunc
-menu opts (KeyDown key) hs = act key'
-  where
-    key' = symKey key
-
-    (_, hf) = head opts
-    (name, _) = opts !! 1
-    rest = (drop 1 . cycle) opts
-
-    act SDL.SDLK_SPACE  = do
-        putStrLn $ name
-        one $ menu rest
-
-    act SDL.SDLK_RETURN = push hf hs
-    act _               = one $ menu rest
-        
-menu opts _             _ = one $ menu opts
-
-
--- Main function {{{1
+-- Main function
 
 main :: IO ()
 main = do
--- Makefile
+++ Makefile
@@ -1,8 +1,8 @@
-all: tetris4d.cabal Main.hs Logic.hs
+all: tetris4d.cabal Main.hs Logic.hs Events.hs
 	cabal configure	
 	cabal build
 	cp dist/build/tetris4d/tetris4d .
 
 clean: 
 	rm tetris4d
-	rm dist
+	rm -rf dist
